2f16e6b445db9314d96ab6f2a896fbc81f6bea72
tedyayu <tedyayu294@gmail.com>
2024-09-15 14:54:28 -0700

    the input is incorporated to ask the amount of squares

--- script.js
+++ script.js
@@ -3,17 +3,26 @@ const container=document.querySelector('.container');
 function displayGrid(){
     let theInput=document.querySelector('.theinput')
     let size=Number(theInput.value);
-    
-    for (let i=0;i<size;i++){
-        let div=document.createElement('div');
-        div.classList.add('grid-item');
-        div.addEventListener('mouseover',()=>{
-            div.style.backgroundColor="blue";
-        })
-        container.appendChild(div);
-    
-        
+    if(size>100){
+        let tehAnswer=document.querySelector('.answer');
+        let newAnswer=document.createElement('p');
+        newAnswer.textContent='can not create the grid please insert a size below 100';
+        tehAnswer.appendChild(newAnswer);
     }
+    else{
+        for (let i=0;i<size*size;i++){
+            let div=document.createElement('div');
+            div.classList.add('grid-item');
+            div.addEventListener('mouseover',()=>{
+                div.style.backgroundColor="blue";
+            })
+            container.appendChild(div);
+        
+            
+        }
+    }
+    
+  
     theInput.value='';
 }
 
